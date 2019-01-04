07307525e59a004464998e7587aa42ffba1250f0
HyunJi-2 <aaa9013@gmail.com>
2019-01-04 17:43:43 +0900

    Updates

---
+++ precache-manifest.cedb7eacb47e1b3d5239fe2f919c5bbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10cf9abb3dd9b21395fc",
+    "url": "/react-tutorial-webtoon/static/js/runtime~main.10cf9abb.js"
+  },
+  {
+    "revision": "7b981742fe7dd596b06d",
+    "url": "/react-tutorial-webtoon/static/js/main.7b981742.chunk.js"
+  },
+  {
+    "revision": "783e417f892c7cef6d29",
+    "url": "/react-tutorial-webtoon/static/js/1.783e417f.chunk.js"
+  },
+  {
+    "revision": "7b981742fe7dd596b06d",
+    "url": "/react-tutorial-webtoon/static/css/main.94ea2f9a.chunk.css"
+  },
+  {
+    "revision": "6e8c1ebbc26e4e94c5e1347df273b117",
+    "url": "/react-tutorial-webtoon/index.html"
+  }
+];
