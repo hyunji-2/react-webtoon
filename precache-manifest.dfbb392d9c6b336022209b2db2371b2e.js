7461c67ebe3fc2e8f3ebca2d51901656f63a353b
HyunJi-2 <aaa9013@gmail.com>
2019-01-08 13:45:35 +0900

    Updates

--- precache-manifest.dfbb392d9c6b336022209b2db2371b2e.js
+++ precache-manifest.dfbb392d9c6b336022209b2db2371b2e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-webtoon/static/js/runtime~main.d0a9ae2d.js"
   },
   {
-    "revision": "51fa33d51ad935156566",
-    "url": "/react-webtoon/static/js/main.51fa33d5.chunk.js"
+    "revision": "d583f192fc22eb20904f",
+    "url": "/react-webtoon/static/js/main.d583f192.chunk.js"
   },
   {
     "revision": "783e417f892c7cef6d29",
     "url": "/react-webtoon/static/js/1.783e417f.chunk.js"
   },
   {
-    "revision": "51fa33d51ad935156566",
+    "revision": "d583f192fc22eb20904f",
     "url": "/react-webtoon/static/css/main.94ea2f9a.chunk.css"
   },
   {
-    "revision": "539a4921dd34ad5915ca3339e37deae9",
+    "revision": "4bdab6ebf7106789f35a92445fb21860",
     "url": "/react-webtoon/index.html"
   }
 ];
